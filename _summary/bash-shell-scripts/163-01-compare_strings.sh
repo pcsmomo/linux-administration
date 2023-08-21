febc533de88ff88358eb7caf954287b7a17b8fc3
Noah Kim <dongwoo_333@hanmail.net>
2023-08-22 08:34:48 +0900

    script: compare strings

---
+++ _summary/bash-shell-scripts/163-01-compare_strings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "String1: " str1
+read -p "S†ring2: " str2
+
+if [ "$str1" = "$str2" ]
+then
+  echo "The strings are equal."
+else
+  echo "The strings are not equal."
+fi
+
+if [[ "$str1" == "$str2" ]]
+then
+  echo "The strings are equal."
+else
+  echo "The strings are not equal."
+fi
+
+if [[ "$str1" != "$str2" ]];then
+  echo "The strings are NOT equal."
+fi
